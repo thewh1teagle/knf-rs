8b160fe212f4a88f8e1722a3a71b1c3a0d3bf44f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-08 01:30:08 +0300

    refactor

--- sys/build.rs
+++ sys/build.rs
@@ -84,15 +84,7 @@ fn main() {
 
     if cfg!(windows) {
         config.static_crt(cfg!(feature = "msvc-static"));
-        config.define(
-            "BUILD_SHARED_LIBS",
-            if cfg!(feature = "msvc-static") {
-                // Don't build shared libraries if msvc-static feature set
-                "OFF"
-            } else {
-                "ON"
-            },
-        );
+        debug_log!("MSVC_STATIC: {}", cfg!(feature = "msvc-static"));
     }
 
     config
